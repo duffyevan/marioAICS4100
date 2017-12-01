86b306d2f5d00368a1be6b4123d4967c9644f3db
Evan Duffy <elduffy@wpi.edu>
2017-12-01 11:31:41 -0500

    Set up skeleton of my AI

--- src/ch/idsia/ai/agents/ai/MyAIAgent.java
+++ src/ch/idsia/ai/agents/ai/MyAIAgent.java
@@ -1,4 +1,33 @@
 package ch.idsia.ai.agents.ai;
 
-public class MyAIAgent {
+import ch.idsia.ai.agents.Agent;
+import ch.idsia.mario.environments.Environment;
+
+public class MyAIAgent implements Agent{
+    private String name = "MyAIAgent";
+
+    @Override
+    public void reset() {
+        // TODO
+    }
+
+    @Override
+    public boolean[] getAction(Environment observation) {
+        return new boolean[0]; // TODO
+    }
+
+    @Override
+    public AGENT_TYPE getType() {
+        return AGENT_TYPE.AI;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 }
