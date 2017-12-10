641856e8dc81c4c84ea5c393d9fea4d537564b4b
Evan Duffy <elduffy@wpi.edu>
2017-12-10 13:49:17 -0500

    Populating the EvolvableAgent inherited methods

--- src/ch/idsia/ai/agents/ai/EvolvableAgent.java
+++ src/ch/idsia/ai/agents/ai/EvolvableAgent.java
@@ -4,6 +4,8 @@ import ch.idsia.ai.Evolvable;
 import ch.idsia.ai.agents.Agent;
 import ch.idsia.mario.environments.Environment;
 
+import java.util.Random;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Sergey Karakovskiy
@@ -11,48 +13,74 @@ import ch.idsia.mario.environments.Environment;
  * Time: 12:30:41 AM
  * Package: ch.idsia.ai.agents.ai;
  */
-public class EvolvableAgent implements Agent,Evolvable
-{
-    protected boolean action[] = new boolean[Environment.numberOfButtons];
-    protected String name = "Instance_of_BasicAIAgent._Change_this_name";
+public class EvolvableAgent implements Agent, Evolvable {
+    private final int numberOfActions = 100;
+    private final float chanceToMutateFrame = 0.05f;
+    private final float chanceToChangeGivenAction = 0.4f;
+
+    private Random random = new Random();
+    private int frameCounter = 0;
+    private boolean actions[][] = new boolean[numberOfActions][Environment.numberOfButtons];
+    protected String name = "Evolvable Agent";
 
 
     @Override
     public Evolvable getNewInstance() {
-        return null;
+        return new EvolvableAgent();
     }
 
     @Override
     public Evolvable copy() {
-        return null;
+        return new EvolvableAgent(actions);
     }
 
-    public EvolvableAgent(String s)
-    {
+    public EvolvableAgent(String s) {
         setName(s);
     }
 
-    public void reset()
-    {
-        action = new boolean[Environment.numberOfButtons];// Empty action
+    public EvolvableAgent(boolean actions[][]) {
+        this.actions = actions;
+    }
+
+    private EvolvableAgent() {
+        for (boolean a[] : actions) {
+            for (boolean b : a) { // randomly change frame
+                b = random.nextBoolean();
+            }
+        }
+    }
+
+    public void reset() {
+        frameCounter = 0;// Empty action
     }
 
     @Override
     public void mutate() {
-
+        for (boolean a[] : actions) {
+            if (random.nextFloat() < chanceToMutateFrame) {
+                for (boolean b : a) { // randomly change frame
+                    if (random.nextFloat() < chanceToChangeGivenAction) {
+                        b = !b;
+                    }
+                }
+            }
+        }
     }
 
-    public boolean[] getAction(Environment observation)
-    {
-        return new boolean[Environment.numberOfButtons]; // Empty action
+    public boolean[] getAction(Environment observation) {
+        return actions[frameCounter++];
+//        return new boolean[Environment.numberOfButtons]; // Empty action
     }
 
-    public AGENT_TYPE getType()
-    {
+    public AGENT_TYPE getType() {
         return AGENT_TYPE.AI;
     }
 
-    public String getName() {        return name;    }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String Name) { this.name = Name;    }
+    public void setName(String Name) {
+        this.name = Name;
+    }
 }
